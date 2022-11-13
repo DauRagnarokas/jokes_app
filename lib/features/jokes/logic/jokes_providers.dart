0e8f037d04e980a2cd42388ec035ed11aba7a00d
Daugirdas Rusinas <daugirdas.rusinas@fundvest.eu>
2022-11-13 22:47:11 +0200

    Added cancelToken to Dio providers

--- lib/features/jokes/logic/jokes_providers.dart
+++ lib/features/jokes/logic/jokes_providers.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../models/joke.dart';
 import '../../../models/joke_category.dart';
@@ -15,24 +16,30 @@ final getJokeCategoriesProvider = FutureProvider<List<JokeCategory>>(
 
 final getRandomJokeProvider = FutureProvider.autoDispose<Joke>(
   (ref) async {
+    final cancelToken = CancelToken();
     final repository = ref.watch(jokesRepositoryProvider);
-    final item = await repository.getRandomJoke();
+    final item = await repository.getRandomJoke(cancelToken: cancelToken);
+    ref.onDispose(() => cancelToken.cancel());
     return item;
   },
 );
 
 final getRandomJokeCategoryProvider = FutureProvider.autoDispose.family<Joke, JokeCategory>(
   (ref, category) async {
+    final cancelToken = CancelToken();
     final repository = ref.watch(jokesRepositoryProvider);
-    final item = await repository.getRandomCategoryJoke(category: category);
+    final item = await repository.getRandomCategoryJoke(category: category, cancelToken: cancelToken);
+    ref.onDispose(() => cancelToken.cancel());
     return item;
   },
 );
 
 final searchJokesProvider = FutureProvider.autoDispose.family<List<Joke>, String?>(
   (ref, query) async {
+    final cancelToken = CancelToken();
     final repository = ref.watch(jokesRepositoryProvider);
-    final data = await repository.searchJokes(query: query);
+    final data = await repository.searchJokes(query: query, cancelToken: cancelToken);
+    ref.onDispose(() => cancelToken.cancel());
     return data;
   },
 );
