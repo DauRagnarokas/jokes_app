5494627f4bf2d410365375c3be8349f27617de2d
Daugirdas Rusinas <daugirdas.rusinas@fundvest.eu>
2022-11-15 11:23:54 +0200

    Added Dio baseUrl

--- lib/services/dio_service.dart
+++ lib/services/dio_service.dart
@@ -1,17 +1,16 @@
 import 'package:dio/dio.dart';
 
 class DioService {
-  static const _baseUrl = 'https://api.chucknorris.io/jokes/';
-  static final _instance = Dio(BaseOptions(baseUrl: _baseUrl));
+  static final _instance = Dio(BaseOptions(
+    baseUrl: 'https://api.chucknorris.io/jokes/',
+  ));
 
   Future<List<T>> getCollection<T>({
     required String path,
     required T Function(dynamic data) builder,
   }) async {
     final Response response = await DioService._instance.get(path);
-    final List<T> result = [
-      for (final item in response.data) builder(item)
-    ];
+    final List<T> result = [for (final item in response.data) builder(item)];
     return result;
   }
 
